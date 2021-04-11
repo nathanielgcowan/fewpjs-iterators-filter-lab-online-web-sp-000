fed46b84b0b8ae11d36af768556b060ff897e7c8
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-11 05:13:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+function findMatching(arrayofdrivers, string) {
+  return arrayofdrivers.filter(function (driverName) {
+    return driverName.toLowerCase() === string.toLowerCase();
+  });
+}
+
+function fuzzyMatch (arrayofdrivers, string) {
+  let lengthOfName = string.length;
+  return arrayofdrivers.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === string;
+  });
+}
+
+function matchName (arrayofdrivers, name) {
+  return arrayofdrivers.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
